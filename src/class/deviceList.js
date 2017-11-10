806e74901605819fef3e7f8ece0990f0fc2d6a3b
hackalot805 <shaun@planetharrington.com>
2017-11-09 16:53:21 -0800

    comments; some minor clean up; added required props.

--- src/class/deviceList.js
+++ src/class/deviceList.js
@@ -1,24 +1,34 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import BackItem from './backItem.js';
 import FolderItem from './folderItem.js';
 import DeviceItem from './deviceItem.js';
 
 /**
+ * propTypes - Setup required properties.
+ *
+ * @type {{grabBagData: (*), onOpen: (*)}}
+ */
+const propTypes = {
+    grabBagData: PropTypes.object.isRequired,
+    historyStack: PropTypes.object.isRequired,
+    onChange: PropTypes.func.isRequired
+};
+
+/**
  * deviceList displays the entire catalog of devices by level; supports navigation through levels, including 'back'.
  */
 class deviceList extends Component {
-
     /**
-     * render displays the current category's items.
-     *
-     * The grabBagContainer.genListItem() method is used for item layout.
+     * render() generates the current child items. (folders and devices)
      *
      * @returns {XML} is the content to render; Using React JSX.
      */
     render() {
-        let back = <div />;
+        const {grabBagData, historyStack} = this.props;
 
-        if (this.props.historyStack.length) {
+        let back = <div />;
+        if (historyStack.length) {
             back = <BackItem onBack={this.onBack.bind(this)} />;
         }
 
@@ -28,14 +38,14 @@ class deviceList extends Component {
                     <div className="row" role="row">
                         <div className="container-fluid">
                             {back}
-                            {Object.keys(this.props.grabBagData.currentSubCategories).map((key, index) =>
-                            this.props.grabBagData.currentSubCategories[key].name !== undefined
+                            {Object.keys(grabBagData.currentSubCategories).map((key, index) =>
+                            grabBagData.currentSubCategories[key].name !== undefined
                                 ?
-                                this.props.grabBagData.currentSubCategories[key].children
+                                grabBagData.currentSubCategories[key].children
                                     ?
-                                    <FolderItem key={key} name={this.props.grabBagData.currentSubCategories[key].name} handleOnClick={this.onOpen.bind(this)} img={this.props.grabBagData.currentSubCategories[key].img} />
+                                    <FolderItem key={key} name={grabBagData.currentSubCategories[key].name} handleOnClick={this.onOpen.bind(this)} img={grabBagData.currentSubCategories[key].img} />
                                     :
-                                    <DeviceItem key={key} name={this.props.grabBagData.currentSubCategories[key].name} handleOnClick={this.onAdd.bind(this)} img={this.props.grabBagData.currentSubCategories[key].img} />
+                                    <DeviceItem key={key} name={grabBagData.currentSubCategories[key].name} handleOnClick={this.onAdd.bind(this)} img={grabBagData.currentSubCategories[key].img} />
                                 :
                                 <span key={key} />
                             )}
@@ -47,32 +57,42 @@ class deviceList extends Component {
     }
 
     /**
-     * handleOnClick passes the event up, to be handled by the parent class.
      *
-     * @param keyName {string} is the name of the item clicked on.  TODO: should use the image GUID for this stuff?
-     * @param event {object} is the click event.
+     * @param event
      */
-    handleOnClick(keyName, event) {
-        this.props.onChange('currentCategoryName', keyName);
-        event.preventDefault();
-    }
-
     onBack(event) {
-        this.props.onChange('currentCategoryName', 'back');
+        const {onChange} = this.props;
+
+        onChange('currentCategoryName', 'back');
         event.preventDefault();
     }
 
+    /**
+     *
+     * @param folderName
+     * @param event
+     */
     onOpen(folderName, event) {
-        console.log("open " + folderName);
-        this.props.onChange('currentCategoryName', folderName);
+        const {onChange} = this.props;
+
+        onChange('currentCategoryName', folderName);
         event.preventDefault();
     }
 
+    /**
+     *
+     * @param deviceName
+     * @param event
+     */
     onAdd(deviceName, event) {
-        console.log("add " + deviceName);
-        this.props.onChange('currentCategoryName', deviceName);
+        const {onChange} = this.props;
+
+        onChange('currentCategoryName', deviceName);
         if (event !== null)
             event.preventDefault();
-    }}
+    }
+}
+
+deviceList.propTypes = propTypes;
 
 export default deviceList;
