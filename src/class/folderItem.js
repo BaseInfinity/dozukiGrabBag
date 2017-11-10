d5dc989bdacea8557966f6b6b7fd1dd53b0e8284
hackalot805 <shaun@planetharrington.com>
2017-11-09 16:26:27 -0800

    comments; some minor clean up; added required props.

--- src/class/folderItem.js
+++ src/class/folderItem.js
@@ -1,23 +1,56 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+/**
+ * propTypes - Setup required properties.
+ *
+ * @type {{name: (*), img: (*), handleOnClick: (*)}}
+ */
+const propTypes = {
+    name: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    handleOnClick: PropTypes.func.isRequired
+};
+
+/**
+ * folderItem is used to display a folder item in the device list.
+ *
+ * A 'folder' is defined as any catalog item that does not have children
+ */
 class folderItem extends Component {
+    /**
+     * render() generates the the folder item HTML.
+     *
+     * @returns {XML} is the content to render; Using React JSX.
+     */
     render() {
+        const {name, img} = this.props;
+
         return (
             <div className='col-xs-6 col-sm-4 col-lg-3 dozuki_grabbag_device_list_item_container' key='back'>
-                <div className='dozuki_grabbag_device_list_section_item' name='currentCategory' value={this.props.name} onClick={this.handleOnClick.bind(this)}>
-                    <div className='dozuki_grabbag_device_list_section_item_title dozuki_grabbag_device_list_folder_container' title={this.props.name}>{this.props.name}</div>
+                <div className='dozuki_grabbag_device_list_section_item' name='currentCategory' value={name} onClick={this.handleOnClick.bind(this)}>
+                    <div className='dozuki_grabbag_device_list_section_item_title dozuki_grabbag_device_list_folder_container' title={name}>{name}</div>
                     <div className='dozuki_grabbag_device_list_section_item_body'>
-                        <img className='dozuki_grabbag_device_list_section_item_image dozuki_grabbag_device_list_section_item_folder_image' src={this.props.img} alt='' />
+                        <img className='dozuki_grabbag_device_list_section_item_image dozuki_grabbag_device_list_section_item_folder_image' src={img} alt='' />
                     </div>
                 </div>
             </div>
         )
     }
 
+    /**
+     * handleOnClick() passes the event up to the parent.
+     *
+     * @param event {object} is the click event.
+     */
     handleOnClick(event) {
-        this.props.handleOnClick(this.props.name, event);
+        const {name, handleOnClick} = this.props;
+
+        handleOnClick(name, event);
         event.preventDefault();
     }
 }
+
+folderItem.propTypes = propTypes;
 
 export default folderItem;
