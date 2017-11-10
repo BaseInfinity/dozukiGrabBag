d89b06714a989fd99dba5acd29685fd6158c9f34
hackalot805 <shaun@planetharrington.com>
2017-11-09 16:30:08 -0800

    comments; some minor clean up; added required props.

--- src/class/backItem.js
+++ src/class/backItem.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+/**
+ * propTypes - Setup required properties.
+ *
+ *  @type {{onBack: (*)}}
+ */
+const propTypes = {
+    onBack: PropTypes.func.isRequired
+};
+
+/**
+ * backItem is the 'back' item used in the device list.
+ */
 class backItem extends Component {
+    /**
+     * render() generates the the 'back' item HTML.
+     *
+     * @returns {XML} is the content to render; Using React JSX.
+     */
     render() {
         return (
              <div className='col-xs-6 col-sm-4 col-lg-3 dozuki_grabbag_device_list_item_container' key='back'>
@@ -13,11 +31,19 @@ class backItem extends Component {
             </div>
         )
     }
-    
+
+    /**
+     * handleOnClick() passes the event up to the parent.
+     *
+     * @param event {object} is the click event.
+     */
     handleOnClick(event) {
-        this.props.onBack(event);
+        const {onBack} = this.props;
+        onBack(event);
         event.preventDefault();
     }
 }
+
+backItem.propTypes = propTypes;
 
 export default backItem;
